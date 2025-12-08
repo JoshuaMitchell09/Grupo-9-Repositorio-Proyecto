69d6014f455d7685337d5ef677fcc6b35b227bb0
JoshuaMitchell09 <103707002+JoshuaMitchell09@users.noreply.github.com>
2025-12-08 14:08:22 -0600

    Delete Sandwich Guy Proyecto directory

--- Sandwich Guy Proyecto/NodoTripleta.java
+++
@@ -1,9 +0,0 @@
-public class NodoTripleta {
-    ResultadoPermutacion dato;
-    NodoTripleta izquierda;
-    NodoTripleta derecha;
-
-    public NodoTripleta(ResultadoPermutacion dato) {
-        this.dato = dato;
-    }
-}
